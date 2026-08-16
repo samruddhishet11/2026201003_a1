485fa2c3fd5c51d5b32e093d4972f052b9920e30
Samruddhi <samruddhishet11@gmail.com>
2026-08-16 10:32:44 +0530

    Taking input for student username

--- src/login.cpp
+++ src/login.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include<string>
 
 void showLoginWelcome() 
 {
     std::cout<<"\nStudent Login\n";
     std::cout<<"Please enter your student credentials.\n";
+}
+
+std::string getusername() 
+{
+    std::string uname;
+    std::cout << "Username: ";
+    std::getline(std::cin, uname);
+    return uname;
 }
