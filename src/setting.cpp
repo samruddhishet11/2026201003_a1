3ede991b2009e479b90db76e9f6948dba162055d
Samruddhi <samruddhishet11@gmail.com>
2026-08-16 11:21:50 +0530

    Display Theme:

--- src/setting.cpp
+++ src/setting.cpp
@@ -3,5 +3,6 @@
 void displaysettings()
 {
     std::cout<<"\nPortal Settings\n";
-    std::cout << "Notifications: Enabled\n";
+    std::cout<<"Notifications: Enabled\n";
+    std::cout<<"Theme: Light mode\n";
 }
