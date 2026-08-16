a4713b8a5d4f05d9f1f112b3bace319dcce0d3ad
Samruddhi <samruddhishet11@gmail.com>
2026-08-16 10:59:54 +0530

    Rectify code in profile.cpp

--- src/profile.cpp
+++ src/profile.cpp
@@ -1,14 +1,5 @@
 #include <iostream>
 
-void displayprofile()
-{
-    std::cout<<"\nStudent Profile\n";
-    std::cout<<"Name: Samruddhi\n";
-    std::cout<<"Roll Number: 2026201003\n";
-    course();
-    acadstatus();
-}
-
 void course()
 {
     std::cout<<"Course: Computer Science\n";
@@ -18,4 +9,13 @@ void course()
 void acadstatus()
 {
     std::cout<<"Academic Status: Active\n";
+}
+
+void displayprofile()
+{
+    std::cout<<"\nStudent Profile\n";
+    std::cout<<"Name: Samruddhi\n";
+    std::cout<<"Roll Number: 2026201003\n";
+    course();
+    acadstatus();
 }
