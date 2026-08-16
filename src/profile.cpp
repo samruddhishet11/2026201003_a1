c6da81540c3aed6a7873eb39174196be0e2d3bff
Samruddhi <samruddhishet11@gmail.com>
2026-08-16 10:52:31 +0530

    Add course details to profile

--- src/profile.cpp
+++ src/profile.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 
-void displayprofile() {
-    std::cout << "\nStudent Profile\n";
-    std::cout << "Name: Samruddhi\n";
-    std::cout << "Roll Number: 2026201003\n";
+void displayprofile()
+{
+    std::cout<<"\nStudent Profile\n";
+    std::cout<<"Name: Samruddhi\n";
+    std::cout<<"Roll Number: 2026201003\n";
+}
+
+void course()
+{
+    std::cout<<"Course: Computer Science\n";
+    std::cout<<"Year: First year\n";
 }
