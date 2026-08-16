22bc8bc76fb3c342cfa5d7c6ef5f47d7af82b485
Samruddhi <samruddhishet11@gmail.com>
2026-08-16 09:28:02 +0530

    Add student portal menu options

--- src/main.cpp
+++ src/main.cpp
@@ -10,8 +10,19 @@ void greetuser()
     std::cout<<"Hello, user!";
 }
 
+
+void showmenu() 
+{
+    std::cout << "\n1. Login\n";
+    std::cout << "2. View Profile\n";
+    std::cout << "3. View Dashboard\n";
+    std::cout << "4. View Settings\n";
+    std::cout << "5. Exit\n";
+}
+
 int main() {
     printWelcome();
     greetuser();
+    showmenu();
     return 0;
 }
