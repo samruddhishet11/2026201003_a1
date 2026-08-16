5c782e0a131ae44121df009f11f8d6669c8d63b7
Samruddhi <samruddhishet11@gmail.com>
2026-08-16 09:38:09 +0530

    Option selection for input

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
+#include <string>
 
 void printWelcome() 
 {
-    std::cout << "Welcome to the Student Portal\n";
+    std::cout<<"Welcome to the Student Portal\n";
 }
 
 void greetuser()
@@ -13,16 +14,21 @@ void greetuser()
 
 void showmenu() 
 {
-    std::cout << "\n1. Login\n";
-    std::cout << "2. View Profile\n";
-    std::cout << "3. View Dashboard\n";
-    std::cout << "4. View Settings\n";
-    std::cout << "5. Exit\n";
+    std::cout<<"\n1. Login\n";
+    std::cout<<"2. View Profile\n";
+    std::cout<<"3. View Dashboard\n";
+    std::cout<<"4. View Settings\n";
+    std::cout<<"5. Exit\n";
 }
 
 int main() {
     printWelcome();
     greetuser();
     showmenu();
+    std::string c;
+
+    std::cout<<"Choose an option:";
+    std::getline(std::cin, c);
+    std::cout<<"You selected option "<<c<<".\n";
     return 0;
 }
